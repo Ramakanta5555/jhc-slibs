20179d8e7c9b4ea0fa64d405d1cb74ec7ed191a4
Ramakanta5555 <103567038+Ramakanta5555@users.noreply.github.com>
2023-07-18 01:21:26 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,5 +1,5 @@
-def call(user,ip,credId){
-  sshagent([credId]) {
+def call(user,ip){
+  sshagent(['tomcat-dev']) {
     sh "scp -o StrictHostKeyChecking=no target/hr-api.war  ec2-user@172.31.42.74:/opt/tomcat9/webapps/ "
       sh "ssh ec2-${user}@${ip} /opt/tomcat9/bin/shutdown.sh"
         sh "ssh ec2-user@172.31.42.74 /opt/tomcat9/bin/startup.sh"
